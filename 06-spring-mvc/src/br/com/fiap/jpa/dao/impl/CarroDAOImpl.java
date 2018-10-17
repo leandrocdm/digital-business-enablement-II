2b682c307481be09ad6cd516a056819a51347e78
leandrocdm <leandrocdm@live.com>
2018-10-17 16:47:31 -0300

    Exercicios

---
+++ 06-spring-mvc/src/br/com/fiap/jpa/dao/impl/CarroDAOImpl.java
@@ -0,0 +1,9 @@
+package br.com.fiap.jpa.dao.impl;
+
+import br.com.fiap.jpa.dao.CarroDAO;
+import br.com.fiap.spring.model.Carro;
+
+public class CarroDAOImpl extends GenericDAOImpl<Carro, Integer>
+														implements CarroDAO{
+
+}
---
+++ 06-spring-mvc/src/br/com/fiap/spring/model/Carro.java
@@ -0,0 +1,62 @@
+package br.com.fiap.spring.model;
+
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name="T_CARRO")
+@SequenceGenerator(name="carro",sequenceName="SQ_T_CARRO",allocationSize=1)
+public class Carro {
+
+	@Id
+	@GeneratedValue(generator="carro",strategy=GenerationType.SEQUENCE)
+	private int codigo;
+	
+	private String modelo;
+	
+	private boolean completo;
+	
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	private Calendar dataFabricacao;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public boolean isCompleto() {
+		return completo;
+	}
+
+	public void setCompleto(boolean completo) {
+		this.completo = completo;
+	}
+
+	public Calendar getDataFabricacao() {
+		return dataFabricacao;
+	}
+
+	public void setDataFabricacao(Calendar dataFabricacao) {
+		this.dataFabricacao = dataFabricacao;
+	}
+
+}
