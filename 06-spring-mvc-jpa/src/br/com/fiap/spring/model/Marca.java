2e247d11376945795ec90d947a16901e3666c8a2
leandrocdm <leandrocdm@live.com>
2018-11-08 16:00:07 -0200

    Corrigindo e melhorias

---
+++ 06-spring-mvc-jpa/src/br/com/fiap/spring/model/Marca.java
@@ -0,0 +1,54 @@
+package br.com.fiap.spring.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name="T_06_DBE2_MARCA")
+@SequenceGenerator(name="marca", sequenceName="SQ_06_DBE2_MARCA",allocationSize=1)
+public class Marca {
+
+	@Id
+	@GeneratedValue(generator="marca",strategy=GenerationType.SEQUENCE)
+	private int codigo;
+	
+	@NotBlank(message="O nome � obrigat�rio")
+	private String nome;
+	
+	@OneToMany(mappedBy="marca")
+	private List<Carro> carros;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public List<Carro> getCarros() {
+		return carros;
+	}
+
+	public void setCarros(List<Carro> carros) {
+		this.carros = carros;
+	}
+	
+}
--- 08-exercicio-ps/src/br/com/fiap/spring/dao/GeneroDAO.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.spring.dao;
-
-import br.com.fiap.spring.model.Genero;
-
-public interface GeneroDAO extends GenericDAO<Genero, Integer>{
-
-	
-}
